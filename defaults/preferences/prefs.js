4e8240574dd2cc40af273bb05cdef693edb97ff1
joachim.herb <none@none>
2009-08-29 18:55:17 +0000

    Changed default preferences settings

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,11 +1,11 @@
 pref("extensions.CompactHeader.headersize.twolineview", false);
-pref("extensions.CompactHeader.buttons.showonlyicon", false);
+pref("extensions.CompactHeader.buttons.showonlyicon", true);
 
-pref("extensions.CompactHeader.view.compact.displayReply", false);
-pref("extensions.CompactHeader.view.compact.displayForward",  false);
-pref("extensions.CompactHeader.view.compact.displayArchive", false);
-pref("extensions.CompactHeader.view.compact.displayJunk", false);
-pref("extensions.CompactHeader.view.compact.displayTrash", false);
+pref("extensions.CompactHeader.view.compact.displayReply", true);
+pref("extensions.CompactHeader.view.compact.displayForward",  true);
+pref("extensions.CompactHeader.view.compact.displayArchive", true);
+pref("extensions.CompactHeader.view.compact.displayJunk", true);
+pref("extensions.CompactHeader.view.compact.displayTrash", true);
 
 pref("extensions.CompactHeader.view.expanded.displayReply", true);
 pref("extensions.CompactHeader.view.expanded.displayForward",  true);
