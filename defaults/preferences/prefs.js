793b7d0f852dfacb9e4b3151e0a34482b2fc19c6
joachim.herb <none@none>
2009-09-04 19:08:58 +0000

    New version 0.6.1: Move menu buttons to header pane button box in compact header view. Clean up of option dialog box: Use standard labels.

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -10,9 +10,13 @@ pref("extensions.CompactHeader.view.compact.displayForward",  true);
 pref("extensions.CompactHeader.view.compact.displayArchive", true);
 pref("extensions.CompactHeader.view.compact.displayJunk", true);
 pref("extensions.CompactHeader.view.compact.displayTrash", true);
+pref("extensions.CompactHeader.view.compact.displayOtherAction", true);
+pref("extensions.CompactHeader.view.compact.displayTag", true);
 
 pref("extensions.CompactHeader.view.expanded.displayReply", true);
 pref("extensions.CompactHeader.view.expanded.displayForward",  true);
 pref("extensions.CompactHeader.view.expanded.displayArchive", true);
 pref("extensions.CompactHeader.view.expanded.displayJunk", true);
 pref("extensions.CompactHeader.view.expanded.displayTrash", true);
+pref("extensions.CompactHeader.view.expanded.displayOtherAction", true);
+pref("extensions.CompactHeader.view.expanded.displayTag", true);
