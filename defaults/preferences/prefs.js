9c6535ada83028a53a520478cf09d111e3aebdff
joachim.herb <none@none>
2009-12-05 22:23:44 +0000

    New version 1.1.0: Flat buttons in the header pane Additional classes for buttons in header pane.

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,7 +1,7 @@
 pref("extensions.CompactHeader.headersize.twolineview", false);
 pref("extensions.CompactHeader.headersize.linkify", false);
-
 pref("extensions.CompactHeader.headersize.addressstyle", false);
+pref("extensions.CompactHeader.headersize.flatButtons", false);
 
 pref("extensions.CompactHeader.version", "0");
 pref("extensions.CompactHeader.firstrun", true);
