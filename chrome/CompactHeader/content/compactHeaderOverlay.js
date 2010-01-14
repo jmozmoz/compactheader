118afd5b87b6e2d91975e9fb4446f30ab7c257b5
joachim.herb <none@none>
2010-01-14 21:33:28 +0000

    New version 1.1.5beta3: Add buttons of main toolbar to header pane toolbar

--- chrome/CompactHeader/content/compactHeaderOverlay.js
+++ chrome/CompactHeader/content/compactHeaderOverlay.js
@@ -375,7 +375,10 @@ org.mozdev.compactHeader.pane = function() {
     var hdrToolbox = document.getElementById("header-view-toolbox");
     var buttons = ["button-reply", "button-replyall", "button-replylist", 
                    "button-tag", "button-forward", "button-archive", "button-file",
-                   "button-print", "button-mark", "button-starMessages"];
+                   "button-print", "button-mark", "button-starMessages",
+                   "button-newmsg", "button-goback", "button-goforward",
+                   "button-previous", "button-next", "button-compact", 
+                   "button-address", "button-stop", "button-getmsg"];
     var currentSet=hdrToolbar.getAttribute("currentset");
     hdrToolbar.currentSet = currentSet;
     for (var i=0; i<buttons.length; i++) {
