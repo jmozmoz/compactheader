fbb32ac46b509d20748ef0456e878dd6d8decea0
joachim.herb <none@none>
2010-09-05 01:15:49 +0000

    First set of changes for 1.3.0beta1: Add sidebars (does not work for view message in new window) Fixes for Thunderbird 3.2

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -2,6 +2,7 @@ pref("extensions.CompactHeader.headersize.twolineview", false);
 pref("extensions.CompactHeader.headersize.linkify", false);
 pref("extensions.CompactHeader.headersize.addressstyle", false);
 pref("extensions.CompactHeader.headersize.flatButtons", false);
+pref("extensions.CompactHeader.toolbox.position", "hdrToolbox.pos.top");
 
 pref("extensions.CompactHeader.version", "0");
 pref("extensions.CompactHeader.firstrun", true);
