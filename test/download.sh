3bf4bad8647e9d664d11d7ba25c2dd3489043cd3
Joachim.Herb@gmx.de <Joachim.Herb@gmx.de>
2011-07-30 17:57:40 +0200

    Added linux to mozmill script
    --HG--
    branch : mozmill

--- test/download.sh
+++ test/download.sh
@@ -1,24 +1,44 @@
 #! /bin/bash
 
-export FTP_DIR=https://ftp.mozilla.org/pub/mozilla.org/thunderbird/nightly/5.0-candidates/build1/unsigned/win32/en-US/
-export APP=thunderbird-5.0.zip
-export TESTS=thunderbird-5.0.tests.zip
-export XPI=../../../AMO/CompactHeader-1.4.2beta3.xpi
+if [ $OSTYPE == msys ] ; then
+  export FTP_DIR=https://ftp.mozilla.org/pub/mozilla.org/thunderbird/nightly/5.0-candidates/build1/unsigned/win32/en-US/
+  export APP=thunderbird-5.0.zip
+  export TESTS=thunderbird-5.0.tests.zip
+  export XPI=../../../AMO/CompactHeader-1.4.2beta3.xpi
+  export TESTDIR=test-5.0
+  export UNPACK="unzip"
+  export UNPACKARGS="-o"
+  export UNPACKTARGETOPT="-d"
+  export APPBIN="thunderbird.exe"
+elif [[ $OSTYPE == *linux* ]] ; then 
+  export FTP_DIR=https://ftp.mozilla.org/pub/mozilla.org/thunderbird/nightly/5.0-candidates/build1/linux-i686/en-US/
+  export APP=thunderbird-5.0.tar.bz2
+  export TESTS=thunderbird-5.0.tests.zip
+  export XPI=../../../AMO/CompactHeader-1.4.2beta3.xpi
+  export TESTDIR=test-5.0
+  export UNPACK="tar"
+  export UNPACKARGS="xjvf"
+  export UNPACKTARGETOPT="-C"
+  export APPBIN="thunderbird"
+else 
+  echo "OS not supported"
+  return 1;
+fi
+
 
 wget -P ftp -N $FTP_DIR/$APP
 wget -P ftp -N $FTP_DIR/$TESTS
 
-export TESTDIR=test-5.0
 mkdir -p $TESTDIR
 
-unzip -o ftp/$APP -d $TESTDIR
+$UNPACK $UNPACKARGS ftp/$APP $UNPACKTARGETOPT $TESTDIR
 unzip -o ftp/$TESTS -d $TESTDIR -x "*mochitest*" "*xpcshell*"
 
-if [ $OSTYPE="msys" ] ; then
+if [ $OSTYPE == msys ] ; then
   junction -d $TESTDIR/mozmill/compactheader
   junction $TESTDIR/mozmill/compactheader compactheader
 else
-  ln -sfn compactheader $TESTDIR/mozmill/compactheader
+  ln -sfn `pwd`/compactheader $TESTDIR/mozmill/compactheader
 fi
 
 # copy drag'n'drop helpers to shared-modules until they are added to thunderbird source
@@ -27,4 +47,4 @@ cp -v shared-modules/test-mouse-event-helpers.js $TESTDIR/mozmill/shared-modules
 cd $TESTDIR/mozmill
 
 # run all tests
-python runtest.py --binary=../thunderbird/thunderbird.exe  -a $XPI -l log -t compactheader
+python runtest.py --binary=../thunderbird/$APPBIN  -a $XPI -l log -t compactheader
