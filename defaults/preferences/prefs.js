bdce45d0e600b132fd52bc1f4bd8aa7f7273cb2a
joachim <none@none>
2011-08-04 00:38:17 +0200

    Add (hidden) preference to set debug(log)Level and set default value to DEBUG.info
    --HG--
    branch : preferences

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -5,3 +5,4 @@ pref("extensions.CompactHeader.headersize.flatButtons", false);
 
 pref("extensions.CompactHeader.version", "0");
 pref("extensions.CompactHeader.firstrun", true);
+pref("extensions.CompactHeader.debugLevel", 1);
