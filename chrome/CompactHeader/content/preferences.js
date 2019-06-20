89b154b7151afe2956e7916116d397bed15ea1f9
jmozmoz <joachim.herb@gmx.de>
2019-06-20 17:23:00 +0200

    Fix preferences dialog
    see https://developer.thunderbird.net/add-ons/tb68/changes#less-than-prefwindow-greater-than-less-than-prefpane-greater-than-less-than-preferences-greater-than-and-less-than-preference-greater-than

---
+++ chrome/CompactHeader/content/preferences.js
@@ -0,0 +1,19 @@
+Preferences.addAll([
+    { id: "extensions.CompactHeader.headersize.twolineview", type: "bool" },
+    { id: "extensions.CompactHeader.headersize.linkify", type: "bool" },
+    { id: "extensions.CompactHeader.headersize.addressstyle", type: "bool" },
+    { id: "extensions.CompactHeader.headersize.flatButtons", type: "bool" },
+
+    { id: "extensions.CompactHeader.toolbox.position", type: "unichar" },
+
+    { id: "extensions.CompactHeader.header.doubleclick", type: "bool" },
+    { id: "extensions.CompactHeader.header.darkenonfocus", type: "bool" },
+
+    { id: "extensions.CompactHeader.version", type: "unichar" },
+    { id: "extensions.CompactHeader.firstrun", type: "bool" },
+    { id: "extensions.CompactHeader.debugLevel", type: "int" },
+
+    { id: "extensions.CompactHeader.mail:3panecollapseState", type: "bool" },
+    { id: "extensions.CompactHeader.mail:messageWindowcollapseState", type: "bool" },
+
+]);
